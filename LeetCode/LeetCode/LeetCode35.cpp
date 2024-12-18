2f7932316b93abaf50bfac9844ec532c4eada350
Gong Yan <1803250724@qq.com>
2024-12-18 19:56:41 +0800

    LeetCode35

---
+++ LeetCode/LeetCode/LeetCode35.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+
+public int SearchInsert(int[] nums, int target)
+{
+    if (nums.size() == 0) return 0;
+    else
+    {
+        if (target < nums[0]) return 0;
+        else if (target > nums[nums.size() - 1]) return nums.size();
+    }
+    // binary search       
+    int left = 0;
+    int right = nums.size();
+    int mid;
+    while (left < right)
+    {
+        mid = left + (right - left) / 2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) left = mid + 1;
+        else right = mid;
+    }
+    return left;
+}
