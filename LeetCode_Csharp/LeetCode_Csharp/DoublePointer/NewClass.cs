c7e3ce80f9592008e590f1c7a6a4a930e29e3452
Gong Yan <1803250724@qq.com>
2024-12-20 11:02:47 +0800

    test

---
+++ LeetCode_Csharp/LeetCode_Csharp/DoublePointer/NewClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCode_Csharp.DoublePointer
+{
+    public class NewClass
+    {
+        
+    }
+}
