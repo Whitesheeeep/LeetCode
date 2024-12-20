6161d920323be051331871ace2fdb535801cad7e
Gong Yan <1803250724@qq.com>
2024-12-20 10:45:12 +0800

    更新了包，然后就是 LeetCode 283

--- LeetCode_Csharp/LeetCode_Csharp/NewNUnit.cs
+++ LeetCode_Csharp/LeetCode_Csharp/NewNUnit.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LeetCode_Csharp.DoublePointer;
 using NUnit.Framework;
+using NUnit;
 
 namespace LeetCode_Csharp
 {
@@ -16,12 +18,10 @@ namespace LeetCode_Csharp
         [Test]
         public void Test1()
         {
-            LeetCode_Csharp.Solution solution = new LeetCode_Csharp.Solution();
-
-            Assert.AreEqual(true, solution.IsPerfectSquqre_2(16));
-            Assert.AreEqual(true, solution.IsPerfectSquqre_2(25));
-            Assert.AreEqual(true, solution.IsPerfectSquqre_2(64));
-            Assert.AreEqual(false, solution.IsPerfectSquqre_2(195));
+            int[] ints = { 0,1};
+            LeetCode283 leetCode283 = new LeetCode283();
+            leetCode283.MoveZeros(ints);
+            Assert.AreEqual(new int[]{1,0}, ints);
             
         }
     }
