eb92ab22c0b3f758c4395ae44c7b7b7325773923
Gong Yan <1803250724@qq.com>
2024-12-24 16:13:49 +0800

    删除链表的倒数第 N 个节点-双指针的应用

---
+++ LeetCode/LeetCode/LeetCode19_removeNthFromEnd_middle.cpp
@@ -0,0 +1,28 @@
+#include "LinkedListSolution.h"
+
+//时间复杂度：O(n)，空间复杂度：O(1)
+ListNode* LinkedListSolution::removeNthFromEnd(ListNode* head, int n)
+{
+	if (head == nullptr) return nullptr;
+	ListNode* dummyHead = new ListNode(0, head);
+	ListNode* slow = dummyHead;
+	ListNode* fast = dummyHead;
+
+	//快指针先走n步
+	while (n)
+	{
+		fast = fast->next;
+		n--;
+	}
+
+	//快慢指针同时走，直到快指针走到最后一个节点
+	while (fast->next != nullptr)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+
+	slow->next = slow->next->next;
+	return dummyHead->next;
+
+}
---
+++ LeetCode/LeetCode/LinkedListSolution.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef LINKEDLISTSOLUTION_H
+#define LINKEDLISTSOLUTION_H
+#include "ListNode.h"
+
+class LinkedListSolution
+{
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n);
+};
+#endif // !LINKEDLISTSOLUTION_H
+
