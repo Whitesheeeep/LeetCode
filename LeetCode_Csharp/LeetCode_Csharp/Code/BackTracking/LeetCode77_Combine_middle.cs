111abcb57feb4d5d90b72557be040f04202b1ed0
Gong Yan <1803250724@qq.com>
2025-03-20 19:52:55 +0800

    开启回溯

---
+++ LeetCode_Csharp/LeetCode_Csharp/Code/BackTracking/LeetCode77_Combine_middle.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCode_Csharp.Code.BackTracking
+{
+    public class LeetCode77_Combine_middle
+    {
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            List<IList<int>> res = new();
+            List<int> path = [];
+            BackTracking(n, k, res, path, 1);
+            return res;
+        }
+
+
+        /// <param name="left"> 用于标识从传入的数字开始有效的索引</param>
+        private void BackTracking(int n, int k, List<IList<int>> res, List<int> path, int left)
+        {
+            // 终止条件
+            if (path.Count == k)
+            {
+                int[] ints = new int[k];
+                path.CopyTo(ints);
+                res.Add(path);
+                return;
+            }
+
+            for (int i = left; i <= n; i++)
+            {
+                path.Add(i);
+                BackTracking(n, k, res, path, i + 1);
+                path.RemoveAt(path.Count - 1);
+            }
+        }
+    }
+}
--- LeetCode_Csharp/LeetCode_Csharp/NewNUnit.cs
+++ LeetCode_Csharp/LeetCode_Csharp/NewNUnit.cs
@@ -30,20 +30,17 @@ namespace LeetCode_Csharp
         [Test]
         public void Test1()
         {
-            LeetCode_Csharp.Code.BinaryTree.LeetCode236_LowestCommonAncestor_middle test = new LeetCode_Csharp.Code.BinaryTree.LeetCode236_LowestCommonAncestor_middle();
-            TreeNode root = new TreeNode(3);
-            TreeNode p = new TreeNode(5);
-            TreeNode q = new TreeNode(1);
-            root.left = p;
-            root.right = q;
-            p.left = new TreeNode(6);
-            p.right = new TreeNode(2);
-            p.right.left = new TreeNode(7);
-            p.right.right = new TreeNode(4);
-            q.left = new TreeNode(0);
-            q.right = new TreeNode(8);
-            TreeNode result = test.LowestCommonAncestor(root, p, p.right.right);
-            Assert.AreEqual(5, result.val);
+            List<IList<int>> res = new();
+            List<int> path = [];
+            path.Add(0);
+            path.Add(1);
+            res.Add(path);
+            path.RemoveAt(1);
+            foreach(var item in res)
+            {
+                foreach(int i in item)
+                    System.Console.WriteLine(i);
+            }
 
 
         }
