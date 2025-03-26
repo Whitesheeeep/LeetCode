ff53e66e814a300a2b71efb2ffca6abd154411be
Gong Yan <1803250724@qq.com>
2025-03-26 17:18:50 +0800

    216

--- LeetCode_Csharp/LeetCode_Csharp/Code/BackTracking/LeetCode216_CombinationSum3_middle.cs
+++ LeetCode_Csharp/LeetCode_Csharp/Code/BackTracking/LeetCode216_CombinationSum3_middle.cs
@@ -9,20 +9,33 @@ namespace LeetCode_Csharp.Code.BackTracking
     {
         public IList<IList<int>> CombinationSum3(int k, int n)
         {
-
+            List<int> path = [];
+            List<IList<int>> res = new();
+            int curSum = 0;
+            BackTracking(k, n, path, res, 1, curSum);
+            return res;
         }
 
-        private void BackTracking(int k, int n, List<int> path, List<IList<int>> res, int start)
+        private void BackTracking(int k, int n, List<int> path, List<IList<int>> res, int start, int curSum)
         {
-            if(path.Count == k)
+            if(curSum > n) return;
+
+            if (path.Count == k)
             {
-                res.Add([..path]);
-                return;
+                if(curSum == n)
+                {
+                    res.Add([.. path]);
+                    return;
+                }
             }
 
-            for(int i = start; i < 9 -(k - path.Count)+1; i++)
+            for (int i = start; i <= 9 - (k - path.Count) + 1; i++)
             {
-                
+                path.Add(i);
+                curSum += i;
+                BackTracking(k, n, path, res, i + 1, curSum);
+                curSum -= i;
+                path.RemoveAt(path.Count - 1);
             }
         }
     }
