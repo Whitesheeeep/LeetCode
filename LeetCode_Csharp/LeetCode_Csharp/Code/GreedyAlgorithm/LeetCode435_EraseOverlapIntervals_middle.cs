c24fe7353dce245f942a5527ddce2bac272c0626
Gong Yan <1803250724@qq.com>
2025-05-21 08:21:36 +0800

    实现 LeetCode 435：删除重叠区间的算法，添加 LeetCode 763：分区标签的初步结构

--- LeetCode_Csharp/LeetCode_Csharp/Code/GreedyAlgorithm/LeetCode435_EraseOverlapIntervals_middle.cs
+++ LeetCode_Csharp/LeetCode_Csharp/Code/GreedyAlgorithm/LeetCode435_EraseOverlapIntervals_middle.cs
@@ -1,7 +1,30 @@
 public class LeetCode435_EraseOverlapIntervals_middle
 {
-    public int EraseOverlapIntervals(int[][] intervals) 
+    public int EraseOverlapIntervals(int[][] intervals)
     {
-        
+        if (intervals.Length <= 1) return 0;
+
+        Array.Sort(intervals, (item1, item2) =>
+        {
+            if (item1[0] == item2[0])
+                return item1[1].CompareTo(item2[1]);
+            return item1[0].CompareTo(item2[0]);
+        });
+
+        int count = 0;
+        int /* left = intervals[0][0],  */ right = intervals[0][1];
+
+        // 贪心，让每次删减的区间都是最大的
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            if (intervals[i][0] < right) // 相交
+            {
+                count++;
+                right = Math.Min(intervals[i][1], right);
+            }
+            else right = intervals[i][1];
+        }
+
+        return count;
     }
 }
---
+++ LeetCode_Csharp/LeetCode_Csharp/Code/GreedyAlgorithm/LeetCode763_PartitionLabels_middle.cs
@@ -0,0 +1,15 @@
+namespace LeetCode_Csharp.Code.GreedyAlgorithm
+{
+    public class LeetCode763_PartitionLabels_middle
+    {
+        public IList<int> PartitionLabels(string s)
+        {
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                
+            }
+            return [];
+        }
+    }
+}
