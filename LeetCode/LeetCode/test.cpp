c39b745629d870f2ffe05c3c8cda6ac120871fe3
Gong Yan <1803250724@qq.com>
2025-06-28 13:00:44 +0800

    更新项目文件并实现链表节点移除功能
    在 `LeetCode.vcxproj` 中添加新编译项，包括 `LeetCode19_removeNthFromEnd_middle.cpp` 和 `MyLinkedList.cpp`。
    在 `LeetCode.vcxproj.filters` 中新增 `源文件\LinkedList` 过滤器，并重新分配相关文件到该过滤器。
    在 `test.cpp` 中新增 `removeNthFromEnd` 函数实现，用于从链表中移除倒数第 `n` 个节点，逻辑基于快慢指针。

--- LeetCode/LeetCode/test.cpp
+++ LeetCode/LeetCode/test.cpp
@@ -1,7 +1,7 @@
 #include <unordered_set>
 #include <vector>
 #include <iostream>
-
+#include "ListNode.h"
 
 using namespace std;
 
@@ -9,4 +9,23 @@ int main()
 {
 	cout << "main Ö´ÐÐ" << endl;
 	return 0;
+}
+
+
+ListNode* removeNthFromEnd(ListNode* head, int n)
+{
+	ListNode* dummyHead = new ListNode(0,head);
+	ListNode* slow = dummyHead, *fast = dummyHead;
+	while (n--)
+	{
+		fast = fast->next;
+	}
+
+	while (fast != nullptr)
+	{
+		fast = fast->next;
+		slow = slow->next;
+	}
+	slow->next = slow->next->next;
+	return dummyHead->next;
 }
